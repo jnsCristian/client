6fdd3854f2e43d37df61aad3e2eb4b7e68c72531
stefan cristian <st.cristian@yahoo.com>
2017-07-12 14:31:24 +0300

    fixed bug from edit user profile

--- src/main/webapp/resources/app/user/commands/edit.user.command.js
+++ src/main/webapp/resources/app/user/commands/edit.user.command.js
@@ -8,17 +8,26 @@ function EditUserCommand () {
 				
 		};
 		
-		$(".user-data").each(function(){
+/*		$(".user-data").each(function(){
 			
 			var elementName=$(this).find("input").attr("id"),
 			elementValue=$(this).find("input").val();
 			user[elementName]=elementValue||undefined;
-			
+			console.log('intra');
 
-		});
-		
-		
-		console.log(user);
+		});*/
+		var elementName=$(".user-data").find("#email").attr("id"),
+		elementValue=$(".user-data").find("#email").val();
+		user[elementName]=elementValue||undefined;
+		var elementName=$(".user-data").find("#nickName").attr("id"),
+		elementValue=$(".user-data").find("#nickName").val();
+		user[elementName]=elementValue||undefined;
+		var elementName=$(".user-data").find("#firstName").attr("id"),
+		elementValue=$(".user-data").find("#firstName").val();
+		user[elementName]=elementValue||undefined;
+		var elementName=$(".user-data").find("#lastName").attr("id"),
+		elementValue=$(".user-data").find("#lastName").val();
+		user[elementName]=elementValue||undefined;
 		EditUserService(user);
 	})
 	
