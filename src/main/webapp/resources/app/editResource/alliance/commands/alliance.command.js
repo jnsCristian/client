92918748db3b9a73e6d20eb5a5c34d54bea6d349
Georgiana Coca <georgiana.coca@ebs.msft>
2017-08-03 12:52:18 +0300

    edit description alliance

--- src/main/webapp/resources/app/editResource/alliance/commands/alliance.command.js
+++ src/main/webapp/resources/app/editResource/alliance/commands/alliance.command.js
@@ -6,7 +6,9 @@ function AllianceCommand () {
 		GetAllianceService(param);
 		atlantis.eventManger.addListener('get.alliance.success', function() {
 			var alliance = atlantis.entity.alliance;
-			$('.alliance-name').html('<span>Alliance Name </span> <input data-allianceid="'+alliance.id+'" id="name" value="'+alliance.name+'"></input>');	
+			//$('.alliance-name').html('<span>Alliance Name </span> <input data-allianceid="'+alliance.id+'" id="name" value="'+alliance.name+'" ></input>');	
+			$('.alliance-name').html('<span>Alliance Name </span> <input data-allianceid="'+alliance.id+'" id="name" value="'+alliance.name+'" ></input>');	
+			$('.alliance-description').html('<span>Description Name </span> <input data-allianceid="'+alliance.id+'" id="description" value="'+alliance.description+'" ></input>');	
 		});
 	});
 };
