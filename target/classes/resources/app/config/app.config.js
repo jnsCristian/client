90e44b3330c792741d7148a38bacb51735616fcb
stefan cristian <st.cristian@yahoo.com>
2017-08-04 09:17:59 +0300

    ...

--- target/classes/resources/app/config/app.config.js
+++ target/classes/resources/app/config/app.config.js
@@ -4,10 +4,10 @@
 	window.atlantis = window.atlantis || {};
 	
 	atlantis.config = {
-		host : "http://localhost:",
-		port : "8085/",
+		host : "http://10.224.15.195:",
+		port : "9080/",
 	}
-
+	
 	/* global event manager for custom listeners */
 	atlantis.eventManger = new EventManager();
 
