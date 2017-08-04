11090fc97905039f99e57bf855c1baee7a7feee7
Madalina Gheorghiu <madalinagheorghiu95@gmail.com>
2017-08-04 11:43:36 +0300

    add image and skills to troops

--- src/main/webapp/resources/app/building/commands/war.building.command.js
+++ src/main/webapp/resources/app/building/commands/war.building.command.js
@@ -26,18 +26,24 @@ function OpenWarBuildingCommand() {
 		if (troops.troopsDto.length) {
 			for (var i = 0; i < troops.troopsDto.length; i++) {
 				var cost=troops.troopsDto[i].cost;
-				troopMarkup += '<br><div class="troop'+ troops.troopsDto[i].id + '"><td>'+ String(troops.troopsDto[i].name)+ '  </td><td><input type="text" data-troop-id="'+ troops.troopsDto[i].id + '"><button class="button troops-train " data-troop-id="'+ troops.troopsDto[i].id + '">Train</button></td></div>'
+				var skills=troops.troopsDto[i].skills;
+				troopMarkup += '<br><div class="troop'+ troops.troopsDto[i].id + '"><td><img src="resources/img/troopsBuilding/'+ troops.troopsDto[i].id + '.png" class="troop">'+ String(troops.troopsDto[i].name)+ '  </td><td><input type="text" data-troop-id="'+ troops.troopsDto[i].id + '"><button class="button troops-train " data-troop-id="'+ troops.troopsDto[i].id + '">Train</button></td></div>   Cost:'
 				for (var j = 0; j <cost.length; j++) {
-					troopMarkup += '<td>'+ String(cost[j].cost)+ '  </td><td>'+ String(cost[j].resourceName)+ '  </td>'
+					troopMarkup += '<td> '+ String(cost[j].cost)+ '  </td><td>'+ String(cost[j].resourceName)+ '  </td>'
 				}
+				troopMarkup += '<br><td> Skills:'+ String(skills.armor)+ '  armor </td><td>'+ String(skills.speed)+ ' speed  </td>'+ '  </td><td> '+ String(skills.demage)+ ' demage </td>'
+				
 				
 			}
 		}else{
 			var cost=troops.troopsDto.cost;
-			troopMarkup += '<br><div class="troop'+ troops.troopsDto.id + '"><td>'+ String(troops.troopsDto.name)+ '  </td><td><input type="text" data-troop-id="'+ troops.troopsDto.id + '"><button class="button troops-train " data-troop-id="'+ troops.troopsDto.id + '">Train</button></td></div>'
+			console.log(cost);
+			var skills=troops.troopsDto.skills;
+			troopMarkup += '<br><div class="troop'+ troops.troopsDto.id + '"><td><img src="resources/img/troopsBuilding/'+ troops.troopsDto.id + '.png"class="troop">  '+ String(troops.troopsDto.name)+ '  </td><td><input type="text" data-troop-id="'+ troops.troopsDto.id + '"><button class="button troops-train " data-troop-id="'+ troops.troopsDto.id + '">Train</button></td></div>  Cost:'
 			for (var j = 0; j < cost.length; j++) {
-				troopMarkup += '<td>'+ String(cost[j].cost)+ '  </td><td>'+ String(cost[j].resourceName)+ '  </td>'
+				troopMarkup += '<td> '+ String(cost[j].cost)+ '  </td><td> '+ String(cost[j].resourceName)+ '  </td>'
 			}
+			troopMarkup += '<br><td> Skills: '+ String(skills.armor)+ '  armor </td><td>'+ String(skills.speed)+ ' speed  </td>'+ '  </td><td> '+ String(skills.demage)+ ' demage </td>'
 		}
 		$('.troops').html(troopMarkup);
 		atlantis.eventManger.fire('written.all.troops');
