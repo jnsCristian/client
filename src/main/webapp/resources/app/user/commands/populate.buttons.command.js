9fc299b32c7a733f985dba063bcd1eb2923f5330
stefan cristian <st.cristian@yahoo.com>
2017-08-08 13:57:19 +0300

    modified show all users

--- src/main/webapp/resources/app/user/commands/populate.buttons.command.js
+++ src/main/webapp/resources/app/user/commands/populate.buttons.command.js
@@ -4,7 +4,7 @@ function PopulateButtonsCommand () {
 	var alliances = atlantis.entity.alliances.alliances;
 	if(alliances&&alliances.length){
 		for (var i = 0; i < alliances.length; i++) {
-			$('[data-allianceId="' + alliances[i].id + '"]').text(alliances[i].name+' Alliance');
+			$('[data-allianceId="' + alliances[i].id + '"]').text(alliances[i].name+'');
 
 		}
 	}
